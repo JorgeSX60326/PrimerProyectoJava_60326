1badedea44b635926ec2539ac2efd80ba0c80e9c
Jun <Jun@deivydgeorge>
2021-03-01 21:01:10 -0600

    Commit completo

---
+++ src/primerproyectojava_60326/PrimerProyectoJava_60326.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_60326;
+
+/**
+ *
+ * @author Jun
+ */
+public class PrimerProyectoJava_60326 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       // Declaramos tres variables con los siguientes datos: int, char, boolean. 
+       int numero_entero = 448;
+       char Letra = '\u0061';
+       boolean color_amarillo = false;
+       //imprimimos las variables que escribimos.
+       System.out.println(" La variable de int es: " + numero_entero);
+       System.out.println(" La variable de char es: " + Letra);
+       System.out.println(" La variable de boolean es: " + color_amarillo);
+       
+    }
+    
+}
